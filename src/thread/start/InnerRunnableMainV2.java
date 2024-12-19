85d26a16f2c4a613e860d11b2a394efd67a84cef
dkf4929 <sng4929@naver.com>
2024-12-19 15:22:50 +0900

    runnable v1,2,3,4

---
+++ src/thread/start/InnerRunnableMainV2.java
@@ -0,0 +1,28 @@
+package thread.start;
+
+import static util.MyLogger.log;
+
+public class InnerRunnableMainV2 {
+
+    public static void main(String[] args) {
+        log("main() start");
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                log("run()");
+            }
+        };
+
+        new Thread(runnable).start();
+
+        log("main() end");
+    }
+
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            log("run()");
+        }
+    }
+}
